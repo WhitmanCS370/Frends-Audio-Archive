789884fc9d1552322b1eb9b2feb1f3861f53d1f0
John Leeds <leedsjohn6@gmail.com>
2024-02-17 13:18:05 -0800

    set up framework for audio effects

--- src/commands.py
+++ src/commands.py
@@ -2,27 +2,25 @@ import os
 import simpleaudio as sa
 
 
-def playAudio(filename):
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()  # Wait until sound has finished playing
-
-
-def playAudioAsync(filename):
+def playAudio(filename, reverse=False, volume=None, speed=None):
     wave_obj = sa.WaveObject.from_wave_file(filename)
     play_obj = wave_obj.play()
     return play_obj
 
 
-def playSequence(filenames):
+def playAudioWait(filename, reverse=False, volume=None, speed=None):
+    playAudio(filename, reverse=reverse, volume=volume, speed=speed).wait_done()
+
+
+def playSequence(filenames, reverse=False, volume=None, speed=None):
     for fname in filenames:
-        playAudio(str(fname))
+        playAudio(fname, reverse=reverse, volume=volume, speed=speed)
 
 
-def playParallel(filenames):
+def playParallel(filenames, reverse=False, volume=None, speed=None):
     play_objs = []
     for fname in filenames:
-        play_objs.append(playAudioAsync(str(fname)))
+        play_objs.append(playAudio(fname, reverse=reverse, volume=volume, speed=speed))
     for play_obj in play_objs:
         play_obj.wait_done()
 

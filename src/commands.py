d481312381c6352df44104832fb247201ef9ec27
John Leeds <leedsjohn6@gmail.com>
2024-02-18 10:06:22 -0800

    Potential implementation for changing speed

--- src/commands.py
+++ src/commands.py
@@ -1,11 +1,16 @@
+import numpy as np
 import os
 import simpleaudio as sa
 import wave
-import numpy as np
 
 
 def playAudio(filename, reverse=False, volume=None, speed=None):
-    wave_obj = sa.WaveObject.from_wave_file(filename)
+    wave_read = wave.open(filename, "rb")
+    audio_data = wave_read.readframes(wave_read.getnframes())
+    num_channels = wave_read.getnchannels()
+    bytes_per_sample = wave_read.getsampwidth()
+    sample_rate = wave_read.getframerate()
+
     if reverse:
         raise NotImplementedError
         # wave_obj = reverseAudio(wave_obj)
@@ -14,9 +19,9 @@ def playAudio(filename, reverse=False, volume=None, speed=None):
         wave_obj = changeVolume(filename, volume)
 
     if speed is not None and speed > 0:
-        raise NotImplementedError
-        # wave_obj = changeSpeed(wave_obj, speed)
-    
+        sample_rate = int(sample_rate * speed)
+
+    wave_obj = sa.WaveObject(audio_data, num_channels, bytes_per_sample, sample_rate)
     play_obj = wave_obj.play()
     return play_obj
 
@@ -46,17 +51,21 @@ def rename(filename, new_name):
     if len(new_name) < 4 or new_name[-4:] != ".wav":
         new_name += ".wav"
     os.rename(filename, new_name)
-    
 
-def changeVolume(filename, volume): 
-    wave_read = wave.open(filename, 'rb')
+
+def changeVolume(filename, volume):
+    wave_read = wave.open(filename, "rb")
     sample_width = wave_read.getsampwidth()
     sample_rate = wave_read.getframerate()
 
-    audio_data = np.frombuffer(wave_read.readframes(wave_read.getnframes()), dtype=np.int16)
+    audio_data = np.frombuffer(
+        wave_read.readframes(wave_read.getnframes()), dtype=np.int16
+    )
     audio_data = (audio_data * volume).astype(np.int16)
     audio_data = np.clip(audio_data, -32768, 32767)
 
-    wave_obj = sa.WaveObject(audio_data.tobytes(), bytes_per_sample=sample_width, sample_rate=sample_rate)
+    wave_obj = sa.WaveObject(
+        audio_data.tobytes(), bytes_per_sample=sample_width, sample_rate=sample_rate
+    )
 
     return wave_obj

36bf7afc3bab43e93e5ffd6c3f3fd2e417ea3cde
John Leeds <leedsjohn6@gmail.com>
2024-02-18 11:41:04 -0800

    implement audio commands

--- src/commands.py
+++ src/commands.py
@@ -1,22 +1,23 @@
+import audioop
 import os
 import simpleaudio as sa
 import wave
-import numpy as np
 
 
 def playAudio(filename, reverse=False, volume=None, speed=None):
-    wave_obj = sa.WaveObject.from_wave_file(filename)
+    with wave.open(filename, "rb") as wave_read:
+        audio_data = wave_read.readframes(wave_read.getnframes())
+        num_channels = wave_read.getnchannels()
+        bytes_per_sample = wave_read.getsampwidth()
+        sample_rate = wave_read.getframerate()
+
     if reverse:
-        raise NotImplementedError
-        # wave_obj = reverseAudio(wave_obj)
-
-    if volume is not None:
-        wave_obj = changeVolume(filename, volume)
-
+        audio_data = audioop.reverse(audio_data, bytes_per_sample)
+    if volume is not None and volume >= 0:
+        audio_data = audioop.mul(audio_data, bytes_per_sample, volume)
     if speed is not None and speed > 0:
-        raise NotImplementedError
-        # wave_obj = changeSpeed(wave_obj, speed)
-    
+        sample_rate = int(sample_rate * speed)
+    wave_obj = sa.WaveObject(audio_data, num_channels, bytes_per_sample, sample_rate)
     play_obj = wave_obj.play()
     return play_obj
 
@@ -46,17 +47,3 @@ def rename(filename, new_name):
     if len(new_name) < 4 or new_name[-4:] != ".wav":
         new_name += ".wav"
     os.rename(filename, new_name)
-    
-
-def changeVolume(filename, volume): 
-    wave_read = wave.open(filename, 'rb')
-    sample_width = wave_read.getsampwidth()
-    sample_rate = wave_read.getframerate()
-
-    audio_data = np.frombuffer(wave_read.readframes(wave_read.getnframes()), dtype=np.int16)
-    audio_data = (audio_data * volume).astype(np.int16)
-    audio_data = np.clip(audio_data, -32768, 32767)
-
-    wave_obj = sa.WaveObject(audio_data.tobytes(), bytes_per_sample=sample_width, sample_rate=sample_rate)
-
-    return wave_obj

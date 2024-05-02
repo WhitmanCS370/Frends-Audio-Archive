abaf8f86d257c5b3143fe1e141c4e40fba28da15
looko <samueluk@uw.edu>
2024-05-02 14:07:39 -0700

    added a search result component

--- src/search_screen_new.py
+++ src/search_screen_new.py
@@ -4,11 +4,16 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
+from kivy.graphics import Color, Rectangle
 from storage_commander import StorageCommander
+from sqlite_storage import Sqlite
+from commands import Commander
 
-class SearchBoxLayout(BoxLayout):
+class SearchScreenLayout(BoxLayout):
     query = ''
-    def __init__(self, commander):
+    def __init__(self):
         super().__init__(orientation='vertical', padding=50, spacing=10)
         self.commander = commander
         self.add_widget(Label(text="Johns Frends Search", font_size=50))
@@ -32,10 +37,10 @@ class SearchBoxLayout(BoxLayout):
         self.query = value
 
     def search_sound(self, instance):
-        print(self.commander.fuzzySearch(self.query, n=5), flush=True)
-        print("name?: ", self.boxes.name, flush=True)
-        print("Tag?: ", self.boxes.tag, flush=True)
-
+        res = self.commander.fuzzySearch(self.query, n=5)
+        popup = Popup(title='test popup', content=SearchResults(res))
+        popup.open()
+        
 class CheckboxesLayout(BoxLayout):
     name = True
     tag = True
@@ -55,14 +60,64 @@ class CheckboxesLayout(BoxLayout):
     
     def tag_active(self, checkbox_instance, value):
         self.tag = value
+        
+
+class SearchResults(BoxLayout):
+    def __init__(self, results, **kwargs):
+        super(SearchResults, self).__init__(**kwargs, orientation='vertical', padding=20, spacing=20)
 
 
+        for result in results:
+            box = MetadataDisplay(result, name=result.name)
+            self.add_widget(box)
 
-class SearchScreenApp(App):
+class MetadataDisplay(BoxLayout):
+    def __init__(self, dataList, name, **kwargs):
+        super(MetadataDisplay, self).__init__(**kwargs, orientation='vertical')
+        
+        self.name = name
+        with self.canvas:
+            # Set border color
+            Color(118, 120, 140, 0.2)
+            # Draw border
+            self.border = Rectangle(pos=self.pos, size=self.size)
+        
+        # Bind the update_border method to the size and pos properties
+        self.bind(pos=self.update_border, size=self.update_border)
+        
+        # Iterate over the object's elements and add them to the layout
+        soundName = Label(text=f"Name: {dataList.name}")
+        soundAuthor = Label(text=f"Author: {dataList.author}")
+        soundDuration = Label(text=f"Duration: {dataList.duration}")
+        
+        self.add_widget(soundName)
+        self.add_widget(soundAuthor)
+        self.add_widget(soundDuration)
+    
+    def update_border(self, *args):
+        # Update border size and position when widget size or position changes
+        self.border.pos = self.pos
+        self.border.size = self.size
+
+# class SearchScreen(Screen):
+#     def __init__(self, **kwargs):
+#         super(SearchScreen, self).__init__(**kwargs)
+
+#         searchScreen = SearchScreenLayout()
+#         self.add_widget(searchScreen)
+
+
+# class ResultsScreen(Screen):
+#     def __init__(self, results **kwargs):
+#         super(ResultsScreen, self).__init__(**kwargs)
+
+#         resultslist = SearchResults(results)
+
+class SearchPageApp(App):
     def build(self):
-        return SearchBoxLayout()
+        return SearchScreenLayout()
     
 if __name__ == "__main__":
     storage = StorageCommander(Sqlite())
     commander = Commander(storage)
-    SearchScreenApp(commander).run()
+    SearchPageApp().run()
